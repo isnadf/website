3862649671478869421f970ebfb5054f3720fbfd
ahmed <ahmedsalah998.as@gmail.com>
2025-12-26 04:35:17 +0200

    modifications

--- components/HexagonTwo.tsx
+++ components/HexagonTwo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 function index() {
   return (
@@ -6,7 +7,7 @@ function index() {
       className='relative w-full'
       style={{
         aspectRatio: '1213/667',
-        backgroundColor: 'tomato',
+        backgroundColor: '#16a34a',
         maskImage:
           "url(\"data:image/svg+xml,%3Csvg width='221' height='122' viewBox='0 0 221 122' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fillRule='evenodd' clipRule='evenodd' d='M183 4C183 1.79086 184.791 0 187 0H217C219.209 0 221 1.79086 221 4V14V28V99C221 101.209 219.209 103 217 103H182C179.791 103 178 104.791 178 107V118C178 120.209 176.209 122 174 122H28C25.7909 122 24 120.209 24 118V103V94V46C24 43.7909 22.2091 42 20 42H4C1.79086 42 0 40.2091 0 38V18C0 15.7909 1.79086 14 4 14H24H43H179C181.209 14 183 12.2091 183 10V4Z' fill='%23D9D9D9'/%3E%3C/svg%3E%0A\")",
         WebkitMaskImage:
@@ -17,18 +18,14 @@ function index() {
         WebkitMaskSize: 'contain',
       }}
     >
-      <video
-        autoPlay
-        muted
-        loop
-        playsInline
+      <Image
+        src='/aboutCover/1.png'
+        alt='About Us'
+        fill
         className='w-full h-full object-cover'
-      >
-        <source
-          src='https://videos.pexels.com/video-files/7710243/7710243-uhd_2560_1440_30fps.mp4'
-          type='video/mp4'
-        />
-      </video>
+        sizes='(max-width: 1024px) 100vw, 50vw'
+        priority
+      />
     </section>
   );
 }
